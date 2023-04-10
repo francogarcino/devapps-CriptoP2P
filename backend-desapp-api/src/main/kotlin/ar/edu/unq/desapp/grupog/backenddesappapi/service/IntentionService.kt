37be573cd67e58de037284470203e93b56d71610
l_ziege <lucasziegemann@gmail.com>
2023-04-10 04:27:14 -0300

    Add service and DAO of intention

---
+++ backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/service/IntentionService.kt
@@ -0,0 +1,6 @@
+package ar.edu.unq.desapp.grupog.backenddesappapi.service
+
+import ar.edu.unq.desapp.grupog.backenddesappapi.model.Intention
+
+interface IntentionService : CrudService<Intention> {
+}
