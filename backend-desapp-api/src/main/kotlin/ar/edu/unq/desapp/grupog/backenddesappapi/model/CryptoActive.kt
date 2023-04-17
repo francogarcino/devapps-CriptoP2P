f91e834727f2c2762302781209f487c9ea340b3e
Valentin Ferreyra <valen.ferreyra@outlook.com>
2023-04-17 09:07:13 -0300

    change implementation in transaction model

--- backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/model/CryptoActive.kt
+++ backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/model/CryptoActive.kt
@@ -3,5 +3,4 @@ package ar.edu.unq.desapp.grupog.backenddesappapi.model
 class CryptoActive(
     var name: CryptoActiveName, val value: Double
 ) {
-
 }
--- backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/model/Transaction.kt
+++ backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/model/Transaction.kt
@@ -3,30 +3,34 @@ package ar.edu.unq.desapp.grupog.backenddesappapi.model
 import ar.edu.unq.desapp.grupog.backenddesappapi.model.trxHelpers.TrxType
 
 class Transaction(
-        var cryptoActive: CryptoActive,
-        var cryptoAmount: Int,
-        var arsAmount: Double,
-        var user1: User,
-        var user2: User,
-        var typeTransacstion: TrxType
+        var intention: Intention,
+        var user1: User
 ) {
+    lateinit var cryptoActive: CryptoActiveName
+    var cryptoAmount: Int? = null
+    var arsAmount: Double? = null
+    lateinit var typeTransaction: TrxType
+    lateinit var user2: User
     lateinit var address: String
     var cryptoPrice: Double = 0.0
     // cantidad de operaciones realizadas por el usuario
 
-    fun setAddress() {
+    fun setInformation() {
+        cryptoActive = intention.cryptoActive
+        cryptoAmount = intention.cryptoAmount
+        arsAmount = intention.arsAmount
+        typeTransaction = intention.trxType
+        user2 = intention.user
+        cryptoPrice = intention.cryptoPrice
         address = if(typeTransaction == TrxType.SELL) user1.cvu
         else user1.wallet
     }
-
-    fun setCryptoPrice() { cryptoPrice = cryptoActive.value }
 
     // Si la trx es cancelada por un usuario -> descontar 20 pts de reputacion
     // Si la trx es cancelada por el sistema -> nada
     // Si la trx es realizada en 30 min -> sumar 10 puntos
     // Si la trx es realizada en + 30 min -> sumar 5 pts
     init {
-        setAddress()
-        setCryptoPrice()
+        setInformation()
     }
 }
