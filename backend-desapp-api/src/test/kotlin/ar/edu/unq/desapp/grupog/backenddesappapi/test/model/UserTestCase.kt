2f484681fc2cb51f198a995730b555fffed8d5bb
Valentin Ferreyra <valen.ferreyra@outlook.com>
2023-04-19 15:52:33 -0300

    add transaction creation test

--- backend-desapp-api/src/test/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/test/model/UserTestCase.kt
+++ backend-desapp-api/src/test/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/test/model/UserTestCase.kt
@@ -142,10 +142,22 @@ class UserTestCase {
         val user = builder.build()
         val createdIntention = user.createIntention(CryptoActiveName.AAVEUSDT, 2, 2.0, TrxType.SELL)
 
-        Assertions.assertNotNull(createdIntention)
         Assertions.assertEquals(createdIntention.getUser(), user)
+        Assertions.assertTrue(user.intentions.contains(createdIntention))
         Assertions.assertEquals(createdIntention.getCryptoActive(), CryptoActiveName.AAVEUSDT)
         Assertions.assertEquals(createdIntention.getArsAmount(), 1600.0)
     }
 
+    @Test
+    fun userAcceptsAnIntention_andATransactionIsCreated() {
+        val userWhoCreates = builder.withName("Valen").build()
+        val userWhoAccepts = builder.withName("Franco").build()
+
+        val createdIntention = userWhoCreates.createIntention(CryptoActiveName.AAVEUSDT, 2, 2.0, TrxType.SELL)
+        val createdTransaction = userWhoAccepts.beginTransaction(createdIntention)
+
+        Assertions.assertEquals(createdTransaction.intention, createdIntention)
+        Assertions.assertEquals(createdTransaction.user_whoCreate, userWhoCreates)
+        Assertions.assertEquals(createdTransaction.user_whoAccept, userWhoAccepts)
+    }
 }
