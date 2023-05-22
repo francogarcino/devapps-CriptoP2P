1baf3b5ad760946ce67538e2df41851a4c1044da
francogarcino <francogarcino@gmail.com>
2023-05-22 03:24:58 -0300

    Reputation tested

--- backend-desapp-api/src/test/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/test/model/WorkflowTestCase.kt
+++ backend-desapp-api/src/test/kotlin/ar/edu/unq/desapp/grupog/backenddesappapi/test/model/WorkflowTestCase.kt
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDateTime
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -229,6 +230,67 @@ class WorkflowTestCase {
         defaultUser.transferMoneyToBankAccount(trx)
         try { externalUser.releaseCrypto(trx) } catch (e: Throwable) { Assertions.assertEquals(expectedMsg, e.message) }
         try { externalUser.cancelTransaction(trx) } catch (e: Throwable) { Assertions.assertEquals(expectedMsg, e.message) }
+    }
+
+    @Test
+    fun testFlow_WhenCompleteOnTime_ReputationIncreaseIn10() {
+        val defaultUser = userBuilder.build()
+        val anotherUser = userBuilder.withCVU("2222222222222222222222")
+            .withWallet("98798798").withEmail("aRandomEmail@hotmail.com").build()
+
+        val intention = defaultUser.createIntention(CryptoActiveName.ETHUSDT, 20, 1.0, TrxType.BUY)
+        val trx = anotherUser.beginTransaction(intention)
+
+        defaultUser.transferMoneyToBankAccount(trx)
+        anotherUser.releaseCrypto(trx)
+
+        Assertions.assertEquals(10, defaultUser.reputation)
+        Assertions.assertEquals(10, anotherUser.reputation)
+    }
+
+    @Test
+    fun testFlow_WhenCompleteAfterTime_ReputationIncreaseIn10() {
+        val defaultUser = userBuilder.build()
+        val anotherUser = userBuilder.withCVU("2222222222222222222222")
+            .withWallet("98798798").withEmail("aRandomEmail@hotmail.com").build()
+
+        val intention = defaultUser.createIntention(CryptoActiveName.ETHUSDT, 20, 1.0, TrxType.BUY)
+        val trx = anotherUser.beginTransaction(intention)
+        trx.apply { creationDate = LocalDateTime.now().minusDays(5) }
+
+        defaultUser.transferMoneyToBankAccount(trx)
+        anotherUser.releaseCrypto(trx)
+
+        Assertions.assertEquals(5, defaultUser.reputation)
+        Assertions.assertEquals(5, anotherUser.reputation)
+    }
+
+    @Test
+    fun testFlow_WhenCancelReputationDecreaseIn20() {
+        val defaultUser = userBuilder.build()
+        val anotherUser = userBuilder.withCVU("2222222222222222222222")
+            .withWallet("98798798").withEmail("aRandomEmail@hotmail.com").build()
+
+        val intention = defaultUser.createIntention(CryptoActiveName.ETHUSDT, 20, 1.0, TrxType.BUY)
+        val trx = anotherUser.beginTransaction(intention)
+
+        anotherUser.apply { reputation = 45 }
+        anotherUser.cancelTransaction(trx)
+        Assertions.assertEquals(25, anotherUser.reputation)
+    }
+
+    @Test
+    fun testFlow_WhenCancelReputationItDoesntTurnBelow0() {
+        val defaultUser = userBuilder.build()
+        val anotherUser = userBuilder.withCVU("2222222222222222222222")
+            .withWallet("98798798").withEmail("aRandomEmail@hotmail.com").build()
+
+        val intention = defaultUser.createIntention(CryptoActiveName.ETHUSDT, 20, 1.0, TrxType.BUY)
+        val trx = anotherUser.beginTransaction(intention)
+
+        anotherUser.cancelTransaction(trx)
+        Assertions.assertEquals(0, anotherUser.reputation)
+        Assertions.assertEquals(TrxStatus.CANCELLED, trx.status)
     }
 
 }
